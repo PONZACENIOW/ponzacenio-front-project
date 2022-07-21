9a2a7acd0220d6b00821ba62cb47bce2421448d1
PONZACENIOW <ponzacenio.work@gmail.com>
2022-07-20 20:09:34 -0500

    Layout: Se añadió template general

---
+++ src/layouts/home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+const HomeLayout = ({ setup, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{setup.title}</title>
+      </Head>
+      <div className="min-h-screen py-10 2xl:py-20 bg-gradient-to-l from-gray-900 to-gray-800">
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default HomeLayout
